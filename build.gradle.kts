fd209a1d60bc0e5857cd788ec8020395c9b92fe7
corneil_jumpco <corneil@jumpco.io>
2021-05-31 09:27:05 +0200

    updated for local build

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,30 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.3.11"
+    kotlin("jvm") version "1.4.32"
     `maven-publish`
 }
 
 group = "org.jetbrains.kotlin.spec.grammar.tools"
-version = "0.1"
+version = "0.1-OPEN-JUMPCO"
 
-val archivePrefix = "kotlin-grammar-tools"
 val jar: Jar by tasks
 
 repositories {
-    mavenCentral()
     mavenLocal()
+    mavenCentral()
 }
+val fatjar by configurations.creating
 
 dependencies {
     compileOnly(kotlin("stdlib-jdk8"))
-    compile("org.jetbrains.kotlin.spec.grammar:kotlin-grammar-parser:0.1")
+    implementation("org.jetbrains.kotlin.spec.grammar:kotlin-grammar-parser:0.1-OPEN-JUMPCO")
+    fatjar("org.jetbrains.kotlin.spec.grammar:kotlin-grammar-parser:0.1-OPEN-JUMPCO")
 }
 
 publishing {
     publications {
         create<MavenPublication>("maven") {
-            groupId = group as String
-            artifactId = archivePrefix
-            version = version as String
-
             from(components["java"])
         }
     }
@@ -37,14 +34,4 @@ tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-jar.archiveName = "$archivePrefix-$version.jar"
-
-jar.manifest {
-    attributes(
-        mapOf(
-            "Class-Path" to configurations.runtime.files.joinToString(" ") { it.name }
-        )
-    )
-}
-
-jar.from(configurations.runtime.map { if (it.isDirectory) it else zipTree(it) })
+jar.from(configurations["fatjar"].files.map { if (it.isDirectory) it else zipTree(it) })
